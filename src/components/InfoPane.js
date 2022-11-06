712fd9f089f68c940e47939683f953d319468aaa
Callum Mackenzie <callum.alex.mackenzie@gmail.com>
2022-11-06 14:00:14 -0800

    Pre date range change

--- src/components/InfoPane.js
+++ src/components/InfoPane.js
@@ -1,3 +1,4 @@
+import moment from "moment/moment";
 import { data } from "../data/Data";
 export { InfoPane }
 
@@ -13,24 +14,22 @@ const InfoPane = () => {
 }
 
 const Title = ({ style }) => {
-	const today = new Date(),
-		d = today.getDate(),
-		m = today.getMonth(),
-		y = today.getFullYear();
 	return (<>
 		<div className="InfoPaneTitle" style={style}>
-			<span>{intDayToDay(d)} of {intMonthToMonth(m)}, {y}</span>
+			<span>{moment().format("LL")}</span>
 		</div>
 	</>);
 }
 
-const HolidayDigest = ({ name, date, digest, link }) => {
-	const dates = date.split(",")
+const HolidayDigest = ({ name, date, digest, link, countries }) => {
+	const countriesStr = countries.reduce((prev, cur) => prev + ", " + cur);
 	return (<>
-		<span className="digestName">{name}</span>
-		<span className="digestDate"></span>
-		<span className="digestDigest"></span>
-		// TODO: Link?
+		<div className="InfoPaneItem">
+			<span className="digestName">{name}</span>
+			<span className="digestCountry">Observed in: {countriesStr}</span>
+			<span className="digestDate">{datesToDateRangeString(date)}</span>
+			<span className="digestDigest"></span>
+		</div>
 	</>);
 }
 
@@ -40,12 +39,12 @@ const HolidayDigest = ({ name, date, digest, link }) => {
 // Ex. intDayToDay(1) => "1st"
 //     intDayToDay(2) => "2nd"
 const intDayToDay = (i) => {
-	switch (i) {
-		case 1: return "1st";
-		case 2: return "2nd";
-		case 3: return "3rd";
-		default: return i + "th";
-	}
+	const lastDigit = i.toString().slice(-1);
+	const firstDigit = i.toString().charAt(0);
+	if (lastDigit == "1" && firstDigit != 1) return i + "st";
+	if (lastDigit == "2" && firstDigit != 1) return i + "nd";
+	if (lastDigit == "3" && firstDigit != 1) return i + "rd";
+	return i + "th";
 }
 
 // Convert integer month to a string repersentation
@@ -54,4 +53,42 @@ const intDayToDay = (i) => {
 const intMonthToMonth = (i) => {
 	return ["January", "February", "March", "April", "May", "June", "July",
 		"August", "September", "October", "November", "December"][i];
+}
+
+// Convert date string to string of ranges
+const datesToDateRangeString = (date) =>
+	dateRangesToString(datesToRanges(date));
+
+// Convert string list of dates in format MM-DD,MM-DD to range
+// Ex. datesToRanges("12-15,12-16,12-17") => {start: {day: 15, month: 12}, 
+//                                            end: {day: 17, month: 12}}
+const datesToRanges = (date) => {
+	// Parse dates into lists of integer months and days
+	const dates = date.replaceAll("-", "")
+		.split(",")
+		.map(str => moment(str, "YYYYMMDD"));
+	// Generate ranges
+	const ranges = [];
+	let startRange = undefined;
+	dates.forEach((e, i) => {
+		if (startRange == undefined) {
+			startRange = e;
+		}
+		if (e.subtract(startRange) != moment(1, "days") || i == dates.length - 1) {
+			ranges.push({ start: startRange, end: e });
+			startRange = e;
+		}
+	});
+	console.log(dates)
+	return ranges;
+}
+
+// Convert list of ranges to a string representation
+const dateRangesToString = (ranges) => {
+	return ranges.map(e => {
+		let res = intDayToDay(e.start.date()) + " of " + intMonthToMonth(e.start.month());
+		if (e.start != e.end)
+			res += " to the " + intDayToDay(e.end.date()) + " of " + intMonthToMonth(e.end.month());
+		return res;
+	}).reduce((curr, prev) => prev + ", " + curr);
 }
--- src/data/Data.js
+++ src/data/Data.js
@@ -6,15 +6,15 @@ const data = {
 		[
 			{
 				name: "Christmas",
-				date: "12/25",
-				country: ["Canada"],
+				date: "2020-12-25",
+				countries: ["Canada", "US"],
 				digest: "Birth of Jesus Christ ... lorem ipsum ...",
 				link: "https://www.christmas.com"
 			},
 			{
 				name: "Valentine's Day",
-				date: "02/14",
-				country: ["Canada"],
+				date: "2020-02-14",
+				countries: ["Canada"],
 				digest: "Holiday to celebrate love ... St.Valentine ... etc",
 				link: "https://www.valentine.com"
 			}
