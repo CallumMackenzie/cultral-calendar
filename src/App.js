721e93210b9aae0889e6530edbc5d349203892e2
leung147 <ericleung147258369@hotmail.com>
2022-11-06 10:45:26 -0800

    Added Calendar framework

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import { Helmet } from "react-helmet";
-import { Calendar } from "./components/Calendar";
-import { Layout } from "./components/Layout"
-import Split from 'react-split';
+import Calendar from "./components/Calendar";
+import React from 'react';
 import './App.css';
-import { InfoPane } from "./components/InfoPane";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<Helmet>
-					<title>Cultral Calendar</title>
-				</Helmet>
-				<Layout>
-					<Split style={{ display: "flex" }}
-						gutterSize={10}
-						gutterAlign="center"
-						direction="horizontal"
-						cursor="col-resize">
-						<Calendar className="Calendar" />
-						<InfoPane className="InfoPane" />
-					</Split>
-				</Layout>
-			</header>
-		</div>
+		<Calendar/>
 	);
 }
 
