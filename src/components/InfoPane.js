f28d6c834d381b4cbd0f2dca3809eea816bc5047
Callum Mackenzie <callum.alex.mackenzie@gmail.com>
2022-11-06 10:55:03 -0800

    Remove helmet

--- src/components/InfoPane.js
+++ src/components/InfoPane.js
@@ -7,8 +7,19 @@ export { InfoPane }
 const InfoPane = () => {
 	return (<>
 		<div className="InfoPane">
+			<Title style/>
 			{data.holidays.map(holiday => HolidayDigest(holiday))}
 		</div>
+	</>);
+}
+
+const Title = () => {
+	const today = new Date(),
+		d = today.getDate(),
+		m = today.getMonth() + 1,
+		y = today.getFullYear();
+	return (<>
+		<h2>{intDayToDay(d)}</h2>
 	</>);
 }
 
@@ -19,3 +30,15 @@ const HolidayDigest = ({ name, date, digest, link }) => {
 }
 
 // Functions:
+
+// Convert integer day to a string repersentation
+// Ex. intDayToDay(1) => "1st"
+//     intDayToDay(2) => "2nd"
+const intDayToDay = (i) => {
+	switch (i) {
+		case 1: return "1st";
+		case 2: return "2nd";
+		case 3: return "3rd";
+		default: return i + "th";
+	}
+}
